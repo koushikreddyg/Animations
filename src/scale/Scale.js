123445bef7dbcc7d4b903ff5b4661dd82b66aa80
koushikui <koushikreddygunapati@gmail.com>
2018-06-16 16:45:24 -0700

    added scale

--- src/scale/Scale.js
+++ src/scale/Scale.js
@@ -3,9 +3,25 @@ import {View, Text, StyleSheet, Animated, TouchableWithoutFeedback} from 'react-
 
 export default class App extends React.Component{
 
-   
+    state={
+        animation: new Animated.Value(1)
+    }
+
+    startAnimation=()=>{
+        Animated.timing(this.state.animation,{
+            toValue: -2,
+            duration: 1500
+        }).start()
+    }
+    
     render(){
-        
+        const animationStyle={
+            transform:[
+                {
+                    scale: this.state.animation
+                }
+            ]
+        }
         
         return(<View style={styles.container}>
             <TouchableWithoutFeedback onPress={this.startAnimation}>
